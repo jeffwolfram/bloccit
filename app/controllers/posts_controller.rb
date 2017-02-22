39dd58911b88abd8d4290b6369c029876eb81a3a
Jeff Wolfram <jeffwolfram@gmail.com>
2017-02-22 13:40:05 -0800

    not done yet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,19 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
   def show
   end
 
@@ -10,5 +23,7 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+
   end
 end
