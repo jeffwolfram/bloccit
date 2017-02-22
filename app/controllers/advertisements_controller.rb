9f1fe4f312fba8ab9862ec8c07006fc65465eb24
Jeff Wolfram <jeffwolfram@gmail.com>
2017-02-21 18:42:38 -0800

    19-assignment

---
+++ app/controllers/advertisements_controller.rb
@@ -0,0 +1,17 @@
+class AdvertisementsController < ApplicationController
+  def index
+    @advertisements = Advertisement.all
+  end
+
+  def show
+    @advertisements = Advertisement.find(params[:id])
+  end
+
+  def new
+    @advertisements = Advertisement.new
+  end
+
+  def create
+    @advertisements = Advertisement.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'advertisements/index'
+
+  get 'advertisements/show'
+
+  get 'advertisements/new'
+
+  get 'advertisements/create'
+
+  get 'advertisement/index'
+
+  get 'advertisement/show'
+
+  get 'advertisement/new'
+
+  get 'advertisement/create'
+
 resources :posts
 
 get 'about' => 'welcome#about'
