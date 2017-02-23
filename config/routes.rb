674105860d3e052b8d218167b09850f20c5b18f0
Jeff Wolfram <jeffwolfram@gmail.com>
2017-02-23 15:58:24 -0800

    make some chances to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-  get 'advertisements/index'
-
-  get 'advertisements/show'
-
-  get 'advertisements/new'
-
-  get 'advertisements/create'
-
-  get 'advertisement/index'
-
-  get 'advertisement/show'
-
-  get 'advertisement/new'
-
-  get 'advertisement/create'
+  # get 'advertisements/index'
+  #
+  # get 'advertisements/show'
+  #
+  # get 'advertisements/new'
+  #
+  # get 'advertisements/create'
+  #
+  # get 'advertisement/index'
+  #
+  # get 'advertisement/show'
+  #
+  # get 'advertisement/new'
+  # 
+  # get 'advertisement/create'
+  resources :advertisements
 
 resources :posts
 
