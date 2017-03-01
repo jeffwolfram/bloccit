e7929eaa8b9a63d4cf883cfaf26874dfcd93ae98
Jeff Wolfram <jeffwolfram@gmail.com>
2017-03-01 00:26:21 -0800

    20-assignment

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,11 +19,26 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    @questions = Questions.find(params[:id])
+    @question = Question.find(params[:id])
     @question.title = params[:question][:title]
     @question.body = params[:question][:title]
+    if @question.save
+      flash[:notice] = "Question was updated."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving the Question. Please try again."
+      render :edit
+    end
   end
 
   def destroy
+    @question = Question.find(params[:id])
+    if @question.destroy
+      flash[:notice] = "\"#{@question.title}\" was deleted successfully."
+      redirect_to questions_path
+    else
+      flash.now[:alert] = "there was an error deleteing the question"
+      render :show
+    end
   end
 end
