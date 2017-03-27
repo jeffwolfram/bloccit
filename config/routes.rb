d9699402e11a0e7230db9302762c26a4333335a9
Jeff Wolfram <jeffwolfram@gmail.com>
2017-03-27 16:17:39 -0700

    29-assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,18 @@
 Rails.application.routes.draw do
-resources :topics do
-resources :posts, except: [:index]
-resources :advertisements, except: [:index]
+  resources :topics do
+    resources :posts, except: [:index]
+    resources :advertisements, except: [:index]
+  end
 
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
 
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
-end
-
-resources :posts, only: [] do
-  resources :comments, only: [:create, :destroy]
-
-
-
-end
-
-resources :users, only: [:new, :create]
- resources :sessions, only: [:new, :create, :destroy]
-
-get 'about' => 'welcome#about'
-
+  get 'about' => 'welcome#about'
   get 'welcome/contact'
-
   get 'welcome/faq'
-
-root 'welcome#index'
+  root 'welcome#index'
 end
