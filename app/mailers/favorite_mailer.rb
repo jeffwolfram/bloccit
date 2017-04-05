980deae21e7546e30b191fbf79429114dce476ad
Jeff Wolfram <jeffwolfram@gmail.com>
2017-04-05 12:49:12 -0700

    31-assignment

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,7 +2,7 @@ class FavoriteMailer < ApplicationMailer
   default from: "jeffwolfram@gmail.com"
 
   def new_comment(user, post, comment)
-    
+
     headers["Message-ID"] = "<comments/#{comment.id}@afternoon-atoll-44085.com>"
     headers["In-Reply-To"] = "<post/#{post.id}@afternoon-atoll-44085.com>"
     headers["References"] = "<post/#{post.id}@afternoon-atoll-44085.com>"
@@ -13,5 +13,15 @@ class FavoriteMailer < ApplicationMailer
 
      mail(to: user.email, subject: "New comment on #{post.title}")
 
+  end
+  def new_post(user, post)
+    headers["Message_ID"] = "<post/#{post.id}@afternoon-atoll-44085.com>"
+    headers["In-Reply-To"] = "post/#{post.id}@afternoon-atoll-44085.com>"
+    headers["References"] = "post/#{post.id}@afternoon-atoll-44085.com>"
+
+    @user = user
+    @post = post
+
+    mail(to: user.email, subject: "You are now following #{post.title} that you created.")
   end
 end
